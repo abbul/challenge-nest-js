8b4293e3256244a1c609dc106147307843f6c560
Abbul <abbul.rodriguez@comafi.com.ar>
2025-05-22 13:43:56 -0300

    se aplica comando lint.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,10 @@ import { SearchCommon } from 'core/share/domain/search';
 import { UserApiGithub } from 'core/user/infrastructure/user-api-github';
 import { GetUsers } from 'core/user/architecture/get-users';
 import { GetUser } from 'core/user/architecture/get-user';
-import { BodySearchUsers, SearchUsers } from 'core/user/architecture/search-users';
+import {
+  BodySearchUsers,
+  SearchUsers,
+} from 'core/user/architecture/search-users';
 import { CreateFavorite } from 'core/favorite/architecture/create-favorite';
 import { FavoriteApiMemory } from 'core/favorite/infrastructure/favorite-api-memory';
 import { GetFavorites } from 'core/favorite/architecture/get-favorites';
@@ -13,19 +16,33 @@ import { ApiResponse, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly httpAdapterHost: HttpAdapterHost<ExpressAdapter>) { }
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost<ExpressAdapter>,
+  ) {}
 
   @Get('users')
   @ApiQuery({ name: 'per_page', required: false, type: 'number' })
   @ApiQuery({ name: 'page', required: false, type: 'number' })
   @ApiOperation({ summary: 'Get users' })
-  @ApiResponse({ status: 200, description: 'The record has been successfully created.'})
-  async getUsersController(@Query() query: SearchCommon, @Res({ passthrough: true }) res: Response): Promise<any> {
-    const cu = new GetUsers(new UserApiGithub())
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully created.',
+  })
+  async getUsersController(
+    @Query() query: SearchCommon,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<any> {
+    const cu = new GetUsers(new UserApiGithub());
     const result = await cu.execute(query);
 
-    const { httpAdapter } = this.httpAdapterHost
-    httpAdapter.setHeader(res, 'Link', result.links.map((link) => `<${link.href}>; rel="${link.rel}"`).join(', '))
+    const { httpAdapter } = this.httpAdapterHost;
+    httpAdapter.setHeader(
+      res,
+      'Link',
+      result.links
+        .map((link) => `<${link.href}>; rel="${link.rel}"`)
+        .join(', '),
+    );
 
     return result.users;
   }
@@ -33,9 +50,15 @@ export class AppController {
   @Get('user/:username')
   @ApiParam({ name: 'username', required: true, type: 'string' })
   @ApiOperation({ summary: 'Get user' })
-  @ApiResponse({ status: 200, description: 'The record has been successfully created.'})
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully created.',
+  })
   async getUserController(@Param('username') username: string): Promise<any> {
-    const cu = new GetUser(new UserApiGithub(), FavoriteApiMemory.getInstance())
+    const cu = new GetUser(
+      new UserApiGithub(),
+      FavoriteApiMemory.getInstance(),
+    );
     return await cu.execute(username);
   }
 
@@ -44,26 +67,40 @@ export class AppController {
   @ApiQuery({ name: 'per_page', required: false, type: 'number' })
   @ApiQuery({ name: 'page', required: false, type: 'number' })
   @ApiOperation({ summary: 'Search users' })
-  @ApiResponse({ status: 200, description: 'The record has been successfully created.'})
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully created.',
+  })
   async searchUsersController(@Query() body: BodySearchUsers): Promise<any> {
-    const cu = new SearchUsers(new UserApiGithub())
+    const cu = new SearchUsers(new UserApiGithub());
     return await cu.execute(body);
   }
 
   @Post('favorite/:username')
   @ApiParam({ name: 'username', required: true, type: 'string' })
   @ApiOperation({ summary: 'Create favorite' })
-  @ApiResponse({ status: 201, description: 'The record has been successfully created.'})
-  async createFavoritesController(@Param('username') username: string): Promise<any> {
-    const cu = new CreateFavorite(FavoriteApiMemory.getInstance(), new UserApiGithub())
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  async createFavoritesController(
+    @Param('username') username: string,
+  ): Promise<any> {
+    const cu = new CreateFavorite(
+      FavoriteApiMemory.getInstance(),
+      new UserApiGithub(),
+    );
     return await cu.execute(username);
   }
 
   @Get('favorites')
   @ApiOperation({ summary: 'Get favorites' })
-  @ApiResponse({ status: 200, description: 'The record has been successfully created.'})
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully created.',
+  })
   async getFavoritesController(): Promise<any> {
-    const cu = new GetFavorites(FavoriteApiMemory.getInstance())
+    const cu = new GetFavorites(FavoriteApiMemory.getInstance());
     return await cu.execute();
   }
 }
